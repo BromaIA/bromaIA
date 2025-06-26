eaa0079ed4d3aa3f6ed49be56f026846e42769a2
BromaIA <contacto@bromaia.com>
2025-06-26 19:05:11 +0200

    actualizo cambios

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import Header from "./header/Header";
-
-
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,6 +13,13 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
+      <head>
+        <Script
+          strategy="afterInteractive"
+          data-domain="bromaia.com"
+          src="https://plausible.io/js/script.js"
+        />
+      </head>
       <body className={`${inter.className} relative overflow-visible min-h-screen bg-black`}>
         <header />
         {children}
@@ -21,3 +27,4 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     </html>
   );
 }
+
